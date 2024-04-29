f7fc11847fa579af6fcac9579044e13064952170
OscarPollo <oscarporterodelsanluis@gmail.com>
2024-04-29 02:09:31 -0600

    preSelectSignal 2
    Se le puso nombre y logo a la app

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'prueba4',
+  appName: 'Vital Sense',
   webDir: 'www',
   server: {
     androidScheme: 'https'
